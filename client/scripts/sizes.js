67114dc19f05c57f59ad24f8ae2c2e575e491d9d
matveybobrov <matveybobrov01@gmail.com>
2024-04-29 12:28:46 +0300

    Feat: remove right and top margins

--- client/scripts/sizes.js
+++ client/scripts/sizes.js
@@ -1,9 +1,9 @@
 const SIZES_MODULE = (function () {
   const rootWidth = 600
   const rootHeight = 600
-  const marginTop = 10
+  const marginTop = 0
   const marginBottom = 20
-  const marginRight = 10
+  const marginRight = 0
   const marginLeft = 20
   const graphWidth = rootWidth - marginLeft - marginRight
   const graphHeight = rootHeight - marginBottom - marginTop
